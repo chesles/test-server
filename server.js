f2797e1628cc38d6e9196d1386bde426f712cb85
John Chesley <john@chesl.es>
2014-12-22 11:58:02 -0500

    bump hapi to 8.0.0

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var hapi    = require('hapi')
   , handler = require('./request-handler')
 
-var host = ''
+var host = 'localhost'
   , port = 2700
   , options = { load: { sampleInterval: 200 }}
 
-var server = new hapi.Server(host, port)
+var server = new hapi.Server()
+
+server.connection({ port: port })
 server.route(handler)
 
 server.start(function() {
